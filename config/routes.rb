1fb13596377d61bc25ceeffee34378c8c9d1c979
koleshkos <koleshkoser@gmail.com>
2024-03-22 00:33:05 +0300

    added devise with user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+    get '/users/password', to: 'devise/password#new' 
+    get '/users/sign_out', to: 'devise/sessions#destroy' 
+  end
+
+  devise_for :users
+
   get 'home/about'
   resources :posts
 
