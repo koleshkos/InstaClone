afc4da8d7a6d709dfc4b04feaef6f512b225de22
koleshkos <koleshkoser@gmail.com>
2024-03-27 21:38:15 +0300

    add show to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   resources :comments
-  devise_for :users
-
+  
   devise_scope :user do
     get '/users', to: 'devise/registrations#new'
     get '/users/password', to: 'devise/password#new' 
     get '/users/sign_out', to: 'devise/sessions#destroy' 
     get '/users/edit', to: 'devise/registrations#edit'
   end
+  
+  devise_for :users
+
+  resources :users, only: [:show]
 
   get 'home/about'
   get 'posts/myposts'
